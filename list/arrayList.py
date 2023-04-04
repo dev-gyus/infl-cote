3a3d3e46e59c388a36b078b135d3c2ac27f29ec8
GyuHyeong <cjsworbehd13@gmail.com>
2023-04-04 22:33:24 +0900

    1. array list 구현 2. linked list append 구현

---
+++ list/arrayList.py
@@ -0,0 +1,27 @@
+# 완전 탐색
+# def twoSum(nums, target):
+#     n = len(nums)
+#     for i in range(n):
+#         for j in range(i+1, n):
+#             if nums[i] + nums[j] is target:
+#                 return True
+#     return False
+
+# sort & two pointer
+def twoSum(nums, target):
+    l, r = 0, len(nums) - 1
+    nums.sort()
+    print(nums)
+    while l < r:
+        if (nums[l] + nums[r]) > target:
+            r -= 1
+            print(f"l={l}, r={r}")
+        elif (nums[l] + nums[r]) < target:
+            l += 1
+            print(f"l={l}, r={r}")
+        else:
+            return True
+    return False
+
+
+print(twoSum(nums=[4, 1, 9, 7, 5, 3, 16], target=14))
---
+++ list/linkedList.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, value, next):
+        self.value = value
+        self.next = next
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def append(self, value:any):
+        node = Node(value, None)
+        if self.head is None:
+            self.head = node
+            print(self.head.value)
+        else:
+            currentNode = self.head
+            while(currentNode.next):
+                currentNode = currentNode.next
+            currentNode.next = node
+            print(currentNode.next.value)
+
+    def __str__(self):
+        result = '['
+        if self.head is not None:
+            current = self.head
+            buffer = ""
+            while current:
+                buffer += (str(current.value) + ',')
+                current = current.next
+            result += buffer.rstrip(",")
+        result += "]"
+        return result
+
+
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.append(4)
+print(ll)
