318f54485f0aceab9ebcb40fb645c58d8d08c18d
GyuHyeong <cjsworbehd13@gmail.com>
2023-11-27 22:27:47 +0900

    1. 괄호 순서대로 열고 닫는 문제 풀이

---
+++ list/problem/stack/LIFO_1.py
@@ -0,0 +1,18 @@
+# {}, [], () 문제
+# "{}[]()" 만으로 구성되어있는 문자열이 주어졌을때 괄호가 여닫혔는지 확인하는 코드를 작성
+# s = '{{}}[()]' <- return true
+# s = '{]}[()]' <- return false
+
+def isValid(s: str):
+    closer_stack = []
+    for char in s:
+        if char == '(':
+            closer_stack.append(')')
+        elif char == '{':
+            closer_stack.append('}')
+        elif char == '[':
+            closer_stack.append(']')
+        # 닫는 괄호들의 차례인 경우
+        elif len(closer_stack) <= 0 or closer_stack.pop() != char:
+            return False
+    return True
