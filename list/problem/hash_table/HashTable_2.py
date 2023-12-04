288781b6eb5dfc2738f70eb34090043bb3fe3ebe
GyuHyeong <cjsworbehd13@gmail.com>
2023-12-04 22:25:59 +0900

    1. hashtable 이용 두번째 문제 풀이 추가 O(n)

--- list/problem/hash_table/HashTable_2.py
+++ list/problem/hash_table/HashTable_2.py
@@ -4,7 +4,7 @@
 # 배열의 길이: 0<= length <= 10^9
 
 # O(nLog(n))으로 푸는 방식
-def solution(nums: [int]) -> int:
+def solution_nlogn(nums: [int]) -> int:
     if not nums:
         return 0
 
@@ -23,7 +23,40 @@ def solution(nums: [int]) -> int:
             count += 1
     return count + 1
 
+# 0(n)으로 푸는 방식
+def solution_n(nums: [int]):
+    if not nums:
+        return 0
+
+    num_dict = {}
+    first_num = 0
+    for num in nums:
+        num_dict[num] = num + 1
+        if first_num == 0:
+            first_num = num
+        elif first_num > num:
+            first_num = num
+
+    count = 0
+    max_count = 0
+    next_num = num_dict[first_num]
+    for num in num_dict.keys():
+        if next_num in num_dict:
+            next_num += 1
+            count += 1
+            if count > max_count:
+                max_count = int(count)
+        else:
+            next_num = num + 1
+            count = 0
+    return max_count + 1
+
+
+
+
+
 
 if __name__ == '__main__':
-    nums = [2, 5, 4, 3, 1, 7, 9, 8, 6, 7, 8, 10, 1, 13, 100, 200]
-    print(solution(nums))
+    nums = [2, 5, 4, 3, 1, 7, 9, 8, 6, 7, 8, 10, 12, 15, 14, 13, 11, 100, 200]
+    # print(solution_nlogn(nums))
+    print(solution_n(nums))
