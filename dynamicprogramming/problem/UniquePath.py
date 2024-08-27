c08e89b0cf0364c3026f00fc6b04eb81234693b3
GyuHyeong <cjsworbehd13@gmail.com>
2024-08-28 00:10:36 +0900

    1. uniquepath dp bottom-up(bfs) 방식 추가

--- dynamicprogramming/problem/UniquePath.py
+++ dynamicprogramming/problem/UniquePath.py
@@ -5,11 +5,11 @@ grid = [[0] * n for _ in range(m)]
 memoization = [[0] * n for _ in range(m)]
 
 def dfs(x, y):
-    print(f"x={x}, y={y}")
     path_count = 0
     if m-1 == x and n-1 == y:
         return 1
-    if x <= m-1 and y <= n-1 and memoization[x][y] > 0:
+    if x >= m or y >= n: return 0
+    if memoization[x][y] > 0:
         return memoization[x][y]
     if x <= m-1:
         path_count += dfs(x+1,y)
@@ -20,5 +20,22 @@ def dfs(x, y):
 
     return path_count
 
+# dp테이블에서 사실 0번 row의 값들과 0번 column의 값들은 모두 1임
+# 이를 미리 세팅하고, x-1, y-1의 값을 더해주는 방식으로 table을 채워주는 방식으로 진행
+tablelation = [[-1] * n for _ in range(m)]
+def bfs():
+    # 초기값 세팅
+    for y in range(n):
+        tablelation[0][y] = 1
+    for x in range(m):
+        tablelation[x][0] = 1
+
+    for x in range(1, m):
+        for y in range(1, n):
+            tablelation[x][y] = tablelation[x-1][y] + tablelation[x][y-1]
+    return tablelation[m-1][n-1]
+
+
 
 print(dfs(0,0))
+print(bfs())
